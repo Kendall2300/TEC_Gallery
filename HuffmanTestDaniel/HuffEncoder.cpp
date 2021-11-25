7914832b5e44c2602c8f9e31af0d2599e26d5211
umi <jcur1802@gmail.com>
2021-11-24 18:52:02 -0600

    Commit para daniel

---
+++ HuffmanTestDaniel/HuffEncoder.cpp
@@ -0,0 +1,128 @@
+//
+// Created by dlurena24 on 21/11/21.
+//
+
+#include "HuffEncoder.h"
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void HuffEncoder::encode(Node* root, string str, unordered_map<char, string> &huffmanCode)
+{
+    if (root == nullptr)
+        return;
+
+    // found a leaf node
+    if (!root->left && !root->right) {
+        huffmanCode[root->ch] = str;
+    }
+
+    encode(root->left, str + "0", huffmanCode);
+    encode(root->right, str + "1", huffmanCode);
+}
+
+//------------------------------------------------------------------------------------------------------------------------
+void HuffEncoder::decode(Node* root, int &index, string str)
+{
+//    cout << "bruh\n";
+    if (root == nullptr) {
+        return;
+    }
+
+    // found a leaf node
+    if (!root->left && !root->right)
+    {
+        cout << root->ch;
+        return;
+    }
+
+    index++;
+
+    if (str[index] =='0')
+        decode(root->left, index, str);
+    else
+        decode(root->right, index, str);
+}
+
+//---------------------------------------------------------------------------------------------------------------
+string HuffEncoder::buildHuffmanTree(string text)
+{
+    // count frequency of appearance of each character
+    // and store it in a map
+    unordered_map<char, int> freq;
+    for (char ch: text) {
+        freq[ch]++;
+    }
+
+    // Create a priority queue to store live nodes of
+    // Huffman tree;
+    priority_queue<Node*, vector<Node*>, comp> pq;
+
+    // Create a leaf node for each character and add it
+    // to the priority queue.
+    for (auto pair: freq) {
+        pq.push(getNode(pair.first, pair.second, nullptr, nullptr));
+    }
+
+    // do till there is more than one node in the queue
+    while (pq.size() != 1)
+    {
+        // Remove the two nodes of highest priority
+        // (lowest frequency) from the queue
+        Node *left = pq.top();
+        pq.pop();
+        Node *right = pq.top();
+        pq.pop();
+
+        // Create a new internal node with these two nodes
+        // as children and with frequency equal to the sum
+        // of the two nodes' frequencies. Add the new node
+        // to the priority queue.
+        int sum = left->freq + right->freq;
+        pq.push(getNode('\0', sum, left, right));
+    }
+
+
+    // root stores pointer to root of Huffman Tree
+//    map<int, priority_queue<Node*, vector<Node*>, comp>> m;
+//    m.insert(make_pair(1, pq));
+//    priority_queue<Node*, vector<Node*>, comp> ffs = m[1];
+
+
+    root = pq.top();
+
+    // traverse the Huffman Tree and store Huffman Codes
+    // in a map. Also prints them
+    unordered_map<char, string> huffmanCode;
+    encode(root, "", huffmanCode);
+
+
+    cout << "Huffman Codes are :\n" << '\n';
+    for (auto pair: huffmanCode) {
+        cout << pair.first << " " << pair.second << '\n';
+    }
+
+//    cout << "\nOriginal string was :\n" << text << '\n';
+
+    // print encoded string
+    string binCode;
+    for (char ch: text) {
+        binCode += huffmanCode[ch];
+    }
+
+//    cout << "\nEncoded string is :\n" << binCode << '\n';
+
+//----------------------------------------------------------------------------------------------------------------------
+    // traverse the Huffman Tree again and this time
+    // decode the encoded string
+//    int index = -1;
+//    cout << "\nDecoded string is: \n";
+//    while (index < (int)binCode.size() - 2) {
+//        decode(getRoot(), index, binCode);
+//    }
+    return binCode;
+}
+
+
+
+
---
+++ HuffmanTestDaniel/cmake-build-debug/CMakeFiles/HuffmanTest.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/HuffDecoder.cpp" "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/cmake-build-debug/CMakeFiles/HuffmanTest.dir/HuffDecoder.cpp.o"
+  "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/HuffEncoder.cpp" "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/cmake-build-debug/CMakeFiles/HuffmanTest.dir/HuffEncoder.cpp.o"
+  "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/Huffman.cpp" "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/cmake-build-debug/CMakeFiles/HuffmanTest.dir/Huffman.cpp.o"
+  "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/img2stringTraductor.cpp" "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/cmake-build-debug/CMakeFiles/HuffmanTest.dir/img2stringTraductor.cpp.o"
+  "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/main.cpp" "/home/dlurena24/Documents/TEC DOCS/DATOS II/TEC_Gallery/HuffmanTest/cmake-build-debug/CMakeFiles/HuffmanTest.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
