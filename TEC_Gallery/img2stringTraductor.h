03f5d3a23ce2ad874de2ff090fce60c80a9da65c
GabrielCerdas08 <68824308+GabrielCerdas08@users.noreply.github.com>
2021-11-25 13:23:30 -0600

    Qt+opencv

---
+++ TEC_Gallery/img2stringTraductor.h
@@ -0,0 +1,16 @@
+//
+// Created by dlurena24 on 21/11/21.
+//
+
+#ifndef HUFFMANTEST_IMG2STRINGTRADUCTOR_H
+#define HUFFMANTEST_IMG2STRINGTRADUCTOR_H
+
+#include <string>
+
+class img2stringTraductor {
+public:
+    std::string Traslate(int b, int g, int r);
+};
+
+
+#endif //HUFFMANTEST_IMG2STRINGTRADUCTOR_H
